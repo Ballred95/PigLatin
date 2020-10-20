030235f496f42d6abe9a810d448188677e3b3ec7
BlakeAllred <blake.allred95@icloud.com>
2020-10-19 22:56:22 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Piggy from './piggy';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Piggy />
+      </div>
+    );
+  }
+}
